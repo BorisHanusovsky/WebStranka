334d618e6e4497ff7b35b47f1f17f3422e67405d
Boris Hanusovsky <boris.hanusovsky@gmail.com>
2024-09-10 23:34:35 +0200

    implemented language change

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-document.cookie = "language = ESP"
+if (getCookie("language") === "ESP")
+    setLanguageSpanish()
+else if (getCookie("language") === "ENG")
+    setLanguageEnglish()
+
+function getCookie(name) {
+    const value = `; ${document.cookie}`;
+    if(value === null)
+        return null
+    const parts = value.split(`; ${name}=`);
+    if (parts.length === 2) return parts.pop().split(';').shift();
+  }
+
+function setLanguageSpanish(){
+    document.cookie = "language = ESP" 
+    document.getElementById("mainHeader").innerHTML = "Hola"
+    document.getElementById("version").innerHTML = "Versión:"
+}
+
+function setLanguageEnglish(){
+    document.cookie = "language = ENG"
+    document.getElementById("mainHeader").innerHTML = "Hello there"
+    document.getElementById("version").innerHTML = "Version:"
+}
