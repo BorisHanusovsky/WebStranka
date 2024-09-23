2344fe01b324bb1d3520d397ee2d370813e075c5
Boris Hanusovsky <boris.hanusovsky@gmail.com>
2024-09-23 22:53:36 +0200

    cookies and colors

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ function onLoad(){
         setLanguageEnglish()
 	else if (getCookie("language") === "SK")
         setLanguageSlovak()
+
+    if (getCookie("darkMode") === "true")
+        changeMode(true)
+    else  changeMode(false)
 }
 
 
@@ -20,27 +24,34 @@ function setLanguageSlovak(){
     document.cookie = "language = SK"
     document.getElementById("mainHeader").innerHTML = "Ahoj"
     document.getElementById("version").innerHTML = "Verzia:"
+    document.getElementById("headerShoppingList").innerHTML = "Nákupný zoznam"
 }
 
 function setLanguageSpanish(){
     document.cookie = "language = ESP" 
     document.getElementById("mainHeader").innerHTML = "Hola"
     document.getElementById("version").innerHTML = "Versión:"
+    document.getElementById("headerShoppingList").innerHTML = "Lista de compras"
+
 }
 
 function setLanguageEnglish(){
     document.cookie = "language = ENG"
     document.getElementById("mainHeader").innerHTML = "Hello there"
     document.getElementById("version").innerHTML = "Version:"
+    document.getElementById("headerShoppingList").innerHTML = "Shopping list"
+
 }
 
 function changeMode(value){
 	if(value.checked){
+        document.cookie = "darkMode = true"
 		document.body.style.backgroundColor = "black"
 		document.body.style.color = "white"
 	}
 	
 	else{
+        document.cookie = "darkMode = false"
 		document.body.style.backgroundColor = "white"
 		document.body.style.color = "black"
 	}
